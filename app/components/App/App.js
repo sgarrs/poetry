9d510c79f0fff48451ebd422c1913525df075156
Garrett Sanders <sgarrettsanders@gmail.com>
2018-02-03 19:45:50 -0500

    move poem data to json file, programmatically wrap html element around poem array

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import styles from './App.scss';
-
 import Poem from '../Poem/Poem';
 
+const data = require('../../data.json');
+        
+        
 class App extends React.Component {
   render() {
     return (
       <div className={styles.app}>
-        <Poem />
+        <Poem poem={data} />
       </div>
     )
 
